a2480cfa12a8b820ea59a7936cb378be50137de9
Sarah Stevens <sssharkie1>
2017-03-17 09:48:09 -0400

    initial files to change

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('turkey burger');
+INSERT INTO burgers (burger_name) VALUES ('bacon double cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('blue cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('veggie burger');
+INSERT INTO burgers (burger_name) VALUES ('royale with cheese');
+INSERT INTO burgers (burger_name) VALUES ('crusty burger');
